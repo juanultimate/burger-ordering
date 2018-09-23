aaa307f601d1de64716b724e4b7c800ef2fe01cf
Juan Gabriel Guzman <jguzman@ioet.com>
2018-09-22 22:14:03 -0300

    Adding burger icon

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -5,11 +5,15 @@ import classes from './SideDrawer.css'
 import Backdrop from "../../UI/Backdrop/Backdrop";
 import Aux from "../../../hoc/Aux";
 const SideDrawer = (props) => {
+    let attachedClasses = [ classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [ classes.SideDrawer, classes.Open];
+    }
 
     return (
         <Aux>
-            <Backdrop show/>
-            <div className={classes.SideDrawer}>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}><Logo/></div>
                 <nav>
                     <NavigationItems/>
